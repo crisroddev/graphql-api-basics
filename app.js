1ebbda34b096a6c1cc168731f89b20d9cfeaf885
CrisRod <crodriguez85@gmail.com>
2019-09-04 16:51:15 -0400

    createUser with hashed pass

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql')
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
 const User = require('./models/user');
@@ -87,11 +88,21 @@ app.use('/graphql', graphqlHttp({
            return event;
         },
         createUser: args => {
-            const user = new User({
-                email: args.userInput.email,
-                password: args.userInput.password
-            })
-
+            return bcrypt
+                .hash(args.userInput.password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: args.userInput.email,
+                        password: hashedPassword
+                    });
+                    return user.save();
+                })
+                .then(result => {
+                    return {...result._doc, _id: result.id}
+                })
+                .catch(error => {
+                    throw error;
+                })
         }
     },
     graphiql: true
